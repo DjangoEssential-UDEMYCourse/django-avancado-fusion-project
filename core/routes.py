3aeb7179e3f597cda881622db5452ece4547b334
THUNDER MASTER <thndrmaster@gmail.com>
2021-06-22 16:59:47 -0300

    [Aula 52] - Adicionando pagina 404 seguindo modelo base e herança

--- core/routes.py
+++ core/routes.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, P404View
 
 urlpatterns = [
-	path('', IndexView.as_view(), name = 'index'),
+	path('', IndexView.as_view(), name='index'),
+	path('404', P404View.as_view(), name='404'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
 	template_name = 'index.html'
+
+
+class P404View(TemplateView):
+	template_name = '404.html'
