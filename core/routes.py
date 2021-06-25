5756e13d9d9adc3997027630dab1c4e50ef3f4d6
ALEXANDRE SAMPAIO <thndrmater@gmail.com>
2021-06-25 14:56:09 -0300

    [Aula - 53] - Definindo os models

--- core/routes.py
+++ core/routes.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import IndexView, P404View
+from .views import IndexView
 
 urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
-	path('404', P404View.as_view(), name='404'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,6 +4,3 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
 	template_name = 'index.html'
 
-
-class P404View(TemplateView):
-	template_name = '404.html'
